26c398c89f6c3612e4074367b63c4238dc7cda62
karl065 <karlos065@hotmail.com>
2023-07-14 19:40:24 -0500

    Documentación

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
+/* Este código importa el módulo `servidor` del archivo `./src/server` y lo asigna a la constante
+`servidor`. Luego establece la constante `PORT` en 3000. Finalmente, inicia el servidor llamando al
+método `listen` en el objeto `server`, pasando la constante `PORT` y una función de devolución de
+llamada que registra un mensaje en la consola cuando el servidor comienza a escuchar en el puerto
+especificado. */
+
 const server = require('./src/server');
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -2,6 +2,9 @@ const {Router} = require('express');
 const {prueba} = require('../Handlers/PruebaHandler');
 const router = Router();
 
-router.get('/', prueba); // * Mensaje de prueba
+/* `router.get('/', prueba);` está definiendo una ruta para una solicitud GET a la URL raíz ("/") de la
+aplicación. Cuando se realiza una solicitud GET a la URL raíz, se ejecutará la función `prueba` del
+módulo `PruebaHandler`. */
+router.get('/', prueba);
 
 module.exports = router;
