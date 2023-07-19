51d6b2f6bca0647fbe66a43dc4ff4d4f147f3857
catalfacu <cataldof21@gmail.com>
2023-07-19 19:24:31 -0300

    Landing codeando

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import { NavBar } from "./Components";
+import { NavBar,Login,SignUp } from "./Components";
 import { Landing, Home, Detail, Review, Faq, Error, About } from "./Views";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar /> */}
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
         {/* <Route path="/favorites"/> */}
         <Route path="/product/:id" element={<Detail />} />
--- Front/src/Components/index.js
+++ Front/src/Components/index.js
@@ -1,4 +1,6 @@
 import NavBar from "./NavBar/NavBar";
 import Cart from "./Cart/Cart";
+import Login from './Forms/LogIn/Login';
+import SignUp from './Forms/SignUp/SignUp';
 
-export { NavBar, Cart };
+export { NavBar, Cart, Login, SignUp };
