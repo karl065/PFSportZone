0f22fa604d42a35a2df85caffd715ae988de286d
catalfacu <cataldof21@gmail.com>
2023-07-19 19:23:11 -0300

    Creacion del landing

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import { NavBar } from "./Components";
+import { Landing, Home, Detail, Review, Faq, Error, About } from "./Views";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-   <h1>sportZone</h1>
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        {/* <Route path="/favorites"/> */}
+        <Route path="/product/:id" element={<Detail />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
