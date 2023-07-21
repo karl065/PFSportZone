6f8dd9f83f9d012c460a2aa21c59b3f4493dadc2
KevinMCJ <kevin72309@gmail.com>
2023-07-21 11:41:57 -0300

    Funcionalidad basica create product

--- Front/src/Views/Form/ProductCreation/ProductCreation.jsx
+++ Front/src/Views/Form/ProductCreation/ProductCreation.jsx
@@ -1,52 +1,64 @@
-import {Formik, Form, Field, ErrorMessage} from 'formik';
-import * as Yup from 'yup';
-import styles from './ProductCreation.module.css';
-import axios from 'axios';
-import server from '../../../Connections/Server';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import styles from "./ProductCreation.module.css";
+import axios from "axios";
+import server from "../../../Connections/Server";
 
 const initialValues = {
-  article_name: '',
-  selling_price: '',
-  purchase_price: '',
-  stock: '',
-  image: '',
-  description: '',
+  article_name: "",
+  selling_price: "",
+  purchase_price: "",
+  stock: "",
+  image: "",
+  description: "",
 };
 
 export const ProductCreation = () => {
   const SignupSchema = Yup.object().shape({
     article_name: Yup.string()
-      .required('Required')
-      .min(2, 'Too Short!')
-      .max(80, 'Too Long!'),
+      .required("Required product name")
+      .min(2, "Too Short!. At leas 2 characters.")
+      .max(80, "Too Long!. No more than 80 characters."),
     selling_price: Yup.number()
-      .required('Required')
-      .min(0.1, 'Minimum Price = 0.1'),
+      .required("Required selling price")
+      .min(0.1, "Minimum Price: 0.1")
+      .transform((value) => (isNaN(value) ? undefined : Number(value))),
     purchase_price: Yup.number()
-      .required('Required')
-      .min(0.1, 'Minimum Price = 0.1'),
-    stock: Yup.number().required('Required').min(0, 'Stock must be positive'),
+      .required("Required purchase price")
+      .min(0.1, "Minimum Price: 0.1")
+      .transform((value) => (isNaN(value) ? undefined : Number(value))),
+    stock: Yup.number()
+      .integer("Must be an integer")
+      .required("Required stock")
+      .min(0, "Stock must be positive")
+      .transform((value) => (isNaN(value) ? undefined : Number(value))),
     description: Yup.string()
-      .required('Description required')
-      .min(20, 'Too Short!')
-      .max(500, 'Too Long!'),
-    image: Yup.string().required('Image required').url('Not an URL'),
+      .required("Description required")
+      .min(20, "Too Short!. At least 20 characters")
+      .max(10000, "Too Long!. No more than 10000 characters."),
+    image: Yup.string().required("Image required").url("Not an URL"),
   });
 
   const handleSubmit = (values) => {
     // ! Aun no esta el endpoint del producto pero esta era la plantilla que tenia
     // a agregar status: "Available"
     try {
+      dispatch(setLoading(true));
       axios
         .post(`${server.api.baseURL}users`, {
           ...values,
-          status: 'Available',
+          status: "Available",
         })
         .then(() => {
-          alert('Product successfully created!');
+          dispatch(setLoading(false));
+          Swal.fire("Good job!", "Successfully register!", "success");
         });
     } catch (error) {
-      alert('Fail creating product. Please try again later.');
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Fail creating a product. Please try again later.",
+      });
     }
   };
 
@@ -57,94 +69,90 @@ export const ProductCreation = () => {
         onSubmit={handleSubmit}
         validationSchema={SignupSchema}
       >
-        {({errors}) => (
+        {({ errors }) => (
           <Form className={styles.form}>
-            <div className={styles.header}>
-              <label>New product</label>
+            <h1 className={styles.title}>NEW PRODUCT</h1>
+            <div className={styles.field}>
+              <label>Name</label>
               <Field
-                name="text"
+                name="article_name"
                 placeholder="Product name"
                 className={styles.input}
               />
               <ErrorMessage
-                name="email"
+                name="article_name"
+                component="span"
+                className={styles.error}
+              />
+            </div>
+            <div className={styles.field}>
+              <label>Selling price</label>
+              <Field
+                name="selling_price"
+                placeholder="Selling price"
+                className={styles.input}
+              />
+              <ErrorMessage
+                name="selling_price"
                 component="span"
                 className={styles.error}
               />
             </div>
 
-            <div className={styles.product_window}>
-              <div className={styles.field}>
-                <label>Selling price</label>
-                <Field
-                  name="selling_price"
-                  placeholder="Selling price"
-                  className={styles.input}
-                />
-                <ErrorMessage
-                  name="selling_price"
-                  component="span"
-                  className={styles.error}
-                />
-              </div>
+            <div className={styles.field}>
+              <label>Purchase price</label>
+              <Field
+                name="purchase_price"
+                placeholder="Min 0.1"
+                className={styles.input}
+              />
+              <ErrorMessage
+                name="purchase_price"
+                component="span"
+                className={styles.error}
+              />
+            </div>
+            <div className={styles.field}>
+              <label>Stock</label>
+              <Field
+                name="stock"
+                placeholder="Stock"
+                className={styles.input}
+              />
+              <ErrorMessage
+                name="stock"
+                component="span"
+                className={styles.error}
+              />
+            </div>
 
-              <div className={styles.field}>
-                <label>Purchase price</label>
-                <Field
-                  type="number"
-                  name="purchase_price"
-                  placeholder="Min 0.1"
-                  className={styles.input}
-                />
-                <ErrorMessage
-                  name="purchase_price"
-                  component="span"
-                  className={styles.error}
-                />
-              </div>
-              <div className={styles.field}>
-                <label>Stock</label>
-                <Field
-                  name="stock"
-                  type="number"
-                  placeholder="Stock"
-                  className={styles.input}
-                />
-                <ErrorMessage
-                  name="stock"
-                  component="span"
-                  className={styles.error}
-                />
-              </div>
+            <div className={styles.field}>
+              <label>Image(s)</label>
+              <Field
+                name="image"
+                type="url"
+                placeholder="URL"
+                className={styles.input}
+              />
+              <ErrorMessage
+                name="image"
+                component="span"
+                className={styles.error}
+              />
+            </div>
 
-              <div className={styles.field}>
-                <label>Image(s)</label>
-                <Field
-                  name="image"
-                  type="url"
-                  placeholder="URL"
-                  className={styles.input}
-                />
-                <ErrorMessage
-                  name="image"
-                  component="span"
-                  className={styles.error}
-                />
-              </div>
-
-              <div className={styles.field}>
-                <label>Description</label>
-                <Field
-                  name="description"
-                  placeholder="Product description"
-                  className={styles.input}
-                />
-                <ErrorMessage
-                  name="description"
-                  component="span"
-                  className={styles.error}
-                />
-              </div>
+            <div className={styles.field}>
+              <label>Description</label>
+              <Field
+                name="description"
+                placeholder="Product description"
+                className={styles.input}
+              />
+              <ErrorMessage
+                name="description"
+                component="span"
+                className={styles.error}
+              />
             </div>
 
             <button
