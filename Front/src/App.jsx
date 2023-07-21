430f2c10b3d29e67eec505ecda4b0359409031ac
unknown <robertosaulh@gmail.com>
2023-07-20 23:08:20 -0600

    Error page 90%, App.js error state add

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,22 +1,17 @@
 import {Routes, Route, useLocation} from 'react-router-dom';
 import {NavBar, Login} from './Components';
-import {
-  Landing,
-  Home,
-  Detail,
-  Review,
-  Faq,
-  Error,
-  About,
-  UserRegister,
-  ProductCreation,
-} from './Views';
+import { Landing, Home, Detail, Review, Faq, Error, About, UserRegister, ProductCreation,} from './Views';
+import { useState } from 'react';
 
 function App() {
   const location = useLocation();
+
+  //estado para no mostrar la nav en la pagina 404
+  const [errorPage, setErrorPage] = useState(true)
+
   return (
     <div className="App">
-      {location.pathname !== '/' && <NavBar />}
+      {(location.pathname !== '/' && errorPage) && <NavBar />}
 
       <Routes>
         <Route path="/" element={<Landing />} />
@@ -29,7 +24,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/review" element={<Review />} />
         <Route path="/faq" element={<Faq />} />
-        <Route path="*" element={<Error />} />
+        <Route path="*" element={<Error setErrorPage={setErrorPage}/>} />
       </Routes>
     </div>
   );
--- Front/src/Views/Error/Error.jsx
+++ Front/src/Views/Error/Error.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import style from './Error.module.css'
+import { useNavigate } from 'react-router-dom';
 
-const Error = () => {
+
+const Error = (props) => {
+  
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    props.setErrorPage(false)
+
+    return () => {
+      props.setErrorPage(true)
+    };
+  }, []);
+
   return (
-    <div>Error</div>
+    <main className={style.Errorcontainer}>
+      <div className={style.Cimages}>
+
+      </div>
+      <div className={style.TBcontainer}>
+        <h1>Ups..!</h1>
+        <p>Page not found</p>
+        <div className={style.btns}>
+          <button className={style.btnError} onClick={()=>{navigate('/home')}}>Go Home</button>
+          <button className={style.btnError} onClick={()=>{window.history.back()}}>Go Back</button>
+        </div>
+      </div>
+    </main>
   )
 }
 
