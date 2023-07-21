4cd5b9a51cb8cb79c975be45b49dd1216f2434d0
KevinMCJ <kevin72309@gmail.com>
2023-07-20 23:15:02 -0300

    Implementacion sweetalert y spinner

--- Front/src/Views/Form/UserRegister/UserRegister.jsx
+++ Front/src/Views/Form/UserRegister/UserRegister.jsx
@@ -1,57 +1,70 @@
-import {Formik, Form, Field, ErrorMessage} from 'formik';
-import {Link, useNavigate} from 'react-router-dom';
-import * as Yup from 'yup';
-import styles from './UserRegister.module.css';
-import axios from 'axios';
-import server from '../../../Connections/Server';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Link, useNavigate } from "react-router-dom";
+import { LoadingSpinner } from "../../../Components";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "../../../redux/actions/actions";
+import * as Yup from "yup";
+import styles from "./UserRegister.module.css";
+import Swal from "sweetalert2";
+import axios from "axios";
+import server from "../../../Connections/Server";
 
 const initialValues = {
-  email: '',
-  user: '',
-  password: '',
-  passwordConfirmation: '',
+  email: "",
+  user: "",
+  password: "",
+  passwordConfirmation: "",
 };
 
 export const UserRegister = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const isLoading = useSelector((state) => state.isLoading);
 
   const SignupSchema = Yup.object().shape({
     email: Yup.string()
       .trim()
-      .required('Email is required')
-      .email('Not an email'),
+      .required("Email is required")
+      .email("Not an email"),
     user: Yup.string()
       .trim()
-      .required('Username is required')
-      .min(5, 'Too Short! At least 5 characters')
-      .max(30, 'Too Long! 30 characters maximum'),
+      .required("Username is required")
+      .min(5, "Too Short! At least 5 characters")
+      .max(30, "Too Long! 30 characters maximum"),
     password: Yup.string()
       .trim()
-      .required('Password is required')
-      .matches(/^\S*$/, 'Cannot have spaces')
-      .min(5, 'At least 5 characters'),
-    passwordConfirmation: Yup.string().oneOf(
-      [Yup.ref('password'), null],
-      'Passwords must match'
-    ),
+      .required("Password is required")
+      .matches(/^\S*$/, "Cannot have spaces")
+      .min(5, "At least 5 characters"),
+    passwordConfirmation: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .required("Required confirmation"),
   });
 
   const handleSubmit = (values) => {
-    // ! Funcional pero cuando este el redux cambiarlo a una action thunk.
+    // ! Funcional pero cuando este el redux cambiarlo a una action thunk, el swal dejarlo aca.
+    // ? Implementar como un componente loading que tenga un fondo tipo swal, centrado en la pantalla y cargue un spinner.
     // role: Client userStatus: true
     try {
+      dispatch(setLoading(true));
       axios
         .post(`${server.api.baseURL}users`, {
           ...values,
-          role: 'Cliente',
+          role: "Cliente",
           userStatus: true,
         })
         .then(() => {
-          alert('Successfully registered!');
-          navigate('/home');
+          dispatch(setLoading(false));
+          Swal.fire("Good job!", "Successfully register!", "success").then(() =>
+            navigate("/home")
+          );
         });
     } catch (error) {
-      alert('Registration failed. Please try again later.');
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Registration failed. Please try again later.",
+      });
     }
   };
 
@@ -63,7 +76,7 @@ export const UserRegister = () => {
         onSubmit={handleSubmit}
         validationSchema={SignupSchema}
       >
-        {({errors}) => (
+        {({ errors }) => (
           <>
             <Form className={styles.form}>
               <p className={styles.loginParagraph}>
@@ -136,11 +149,12 @@ export const UserRegister = () => {
               >
                 Submit
               </button>
-              <Link to={'/'}>Back</Link>
+              <Link to={"/"}>Back</Link>
             </Form>
           </>
         )}
       </Formik>
+      {isLoading && <LoadingSpinner />}
     </div>
   );
 };
