63294a0f4503472bcca1ab7fc1062564364bf6e2
karl065 <karlos065@hotmail.com>
2023-07-21 15:41:32 -0500

    Actualización de index routes

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -7,9 +7,6 @@ const filters = require('./FilterRoutes/FilterRoutes.js');
 const category = require('./CategoriesRoutes/CategoriesRoutes.js');
 const router = Router();
 
-/* `router.get('/', prueba);` está definiendo una ruta para una solicitud GET a la URL raíz ("/") de la
-aplicación. Cuando se realiza una solicitud GET a la URL raíz, se ejecutará la función `prueba` del
-módulo `PruebaHandler`. */
 router.use('/users', users);
 router.use('/auth', auth);
 router.use('/inventory', inventory);
