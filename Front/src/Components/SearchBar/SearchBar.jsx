eaada09f21e266899e305b0f4deeb6023ba15feb
KevinMCJ <kevin72309@gmail.com>
2023-07-21 14:36:52 -0300

    Conexion con el inventario de redux y pequeño fix en el estilo

--- Front/src/Components/SearchBar/SearchBar.jsx
+++ Front/src/Components/SearchBar/SearchBar.jsx
@@ -1,43 +1,11 @@
 import {useState} from 'react';
 import styles from './SearchBar.module.css';
-
-// ! Prueba funcionamiento => Borrar luego.
-const products = [
-  {id: 1, name: 'Balón de fútbol'},
-  {id: 2, name: 'Camiseta de baloncesto'},
-  {id: 3, name: 'Raqueta de tenis'},
-  {id: 4, name: 'Pelota de voleibol'},
-  {id: 5, name: 'Guantes de boxeo'},
-  {id: 6, name: 'Patines en línea'},
-  {id: 7, name: 'Saco de boxeo'},
-  {id: 8, name: 'Casco de ciclismo'},
-  {id: 9, name: 'Red de bádminton'},
-  {id: 10, name: 'Pesa rusa'},
-  {id: 11, name: 'Gorra de natación'},
-  {id: 12, name: 'Tabla de surf'},
-  {id: 13, name: 'Gafas de esquí'},
-  {id: 14, name: 'Cuerda de saltar'},
-  {id: 15, name: 'Bicicleta de montaña'},
-  {id: 16, name: 'Botas de fútbol'},
-  {id: 17, name: 'Tablero de ajedrez'},
-  {id: 18, name: 'Pelota de rugby'},
-  {id: 19, name: 'Gorro de waterpolo'},
-  {id: 20, name: 'Balón de béisbol'},
-  {id: 21, name: 'Tabla de snowboard'},
-  {id: 22, name: 'Pelota de golf'},
-  {id: 23, name: 'Cinta de correr'},
-  {id: 24, name: 'Máscara de buceo'},
-  {id: 25, name: 'Patines de hielo'},
-  {id: 26, name: 'Bolso de deporte'},
-  {id: 27, name: 'Muñequeras de tenis'},
-  {id: 28, name: 'Zapatillas de running'},
-  {id: 29, name: 'Balón de balonmano'},
-  {id: 30, name: 'Cuerdas de escalada'},
-];
+import { useSelector } from 'react-redux';
 
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const inventory = useSelector(state => state.inventory);
 
   const handleChange = (event) => {
     setSearchQuery(event.target.value);
@@ -45,8 +13,7 @@ const SearchBar = () => {
   };
 
   const handleSearch = async (query) => {
-    // * Lógica para filtrar las cards en función de la búsqueda => Dispatch Redux
-    // ! Esto seria con dispatch a la action post. No va a tener .results cambiarlo luego.
+    // ! Esto llamaría a un filtrado por nombre del home y..
     // ? Recordar hacerle trim() a la query antes de enviar
     setSearchQuery(query);
     setSearchResults([]);
@@ -61,9 +28,8 @@ const SearchBar = () => {
 
   const filterProducts = () => {
     // * Filtrando productos basados en la lista.
-    // ! Estos products vendrían del estado global [useSelector], faltaría el redux.
-    const filteredProducts = products.filter((product) =>
-      product.name.toLowerCase().includes(searchQuery.toLowerCase().trim())
+    const filteredProducts = inventory.filter((product) =>
+      product.article_name.toLowerCase().includes(searchQuery.toLowerCase().trim())
     );
 
     // * Solo quiero mostrar 6
@@ -92,8 +58,8 @@ const SearchBar = () => {
         {searchResults.length && (
           <ul className={styles.resultsList}>
             {searchResults.map((item) => (
-              <li key={item.id} onClick={() => handleSearch(item.name)}>
-                {item.name}
+              <li key={item.id_inventory} onClick={() => handleSearch(item.article_name)}>
+                {item.article_name}
               </li>
             ))}
           </ul>
