b9c71d866b586efb535e7b099045ccc85758b5dc
KevinMCJ <kevin72309@gmail.com>
2023-07-20 23:42:16 -0300

    prettier

--- Front/src/Components/index.js
+++ Front/src/Components/index.js
@@ -1,6 +1,6 @@
-import NavBar from './NavBar/NavBar';
-import Card from './Card/Card';
-import SearchBar from './SearchBar/SearchBar';
-import LoadingSpinner from './LoadingSpinner/LoadingSpinner';
+import NavBar from "./NavBar/NavBar";
+import Card from "./Card/Card";
+import SearchBar from "./SearchBar/SearchBar";
+import LoadingSpinner from "./LoadingSpinner/LoadingSpinner";
 
-export {NavBar, Card, SearchBar, LoadingSpinner};
+export { NavBar, Card, SearchBar, LoadingSpinner };
