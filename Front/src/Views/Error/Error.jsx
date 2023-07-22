e4892ecc609247d758745964bfd5750a703189c8
R0b3rt0-04 <robertosaulh@gmail.com>
2023-07-22 15:28:06 -0600

    complete errror page

--- Front/src/Views/Error/Error.jsx
+++ Front/src/Views/Error/Error.jsx
@@ -18,7 +18,11 @@ const Error = (props) => {
   return (
     <main className={style.Errorcontainer}>
       <div className={style.Cimages}>
-
+          <img src="https://res.cloudinary.com/dpjeltekx/image/upload/v1689947525/PF/Rocket_gvuj3a.png" className={style.Rocket} />
+          <div className={style.planetContainer}>
+            <img src="https://res.cloudinary.com/dpjeltekx/image/upload/v1689947525/PF/planet_brnqfv.png" className={style.planet} />
+          </div>
+            <img src="https://res.cloudinary.com/dpjeltekx/image/upload/v1689947525/PF/spaceman_t14km3.png" className={style.spaceman} />
       </div>
       <div className={style.TBcontainer}>
         <h1>Ups..!</h1>
