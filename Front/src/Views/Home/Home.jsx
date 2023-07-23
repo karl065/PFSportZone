282d8ed148d14978c39d3fe8b60b7efb0bec556b
catalfacu <cataldof21@gmail.com>
2023-07-23 01:09:33 -0300

    Actualizacion del home

--- Front/src/Views/Home/Home.jsx
+++ Front/src/Views/Home/Home.jsx
@@ -1,24 +1,33 @@
 /* eslint-disable no-unused-vars */
 import {useState} from 'react';
+import {useSelector} from 'react-redux';
 import Styles from './Home.module.css';
 import Pagination from '../../Components/Pagination/Pagination';
 
 const Home = (props) => {
-  const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  //traigo el la propiedad inventory del estado global
+  const inventory = useSelector(state => state.inventory);
+
+  //paginado
   const [page, setPage] = useState(1);
   const [amountPerPage, setAmountPerPage] = useState(3);
-  const pageCount = items.length / amountPerPage;
+  const pageCount = inventory.length / amountPerPage;
 
   return (
     <div className={Styles.container}>
       <div className={Styles.cards}>
-        {items
+        {inventory
           .slice(
             (page - 1) * amountPerPage,
             (page - 1) * amountPerPage + amountPerPage
           )
-          .map((item, index) => {
-            return <h3 key={index}>{item}</h3>;
+          .map((article, index) => {
+            return <div key={index}>
+                    <img src={article.image} alt={article.article_name} />
+                    <h2 >{article.article_name}</h2>
+                    <h3>{article.selling_price}</h3>
+              </div>
+
           })}
       </div>
 
