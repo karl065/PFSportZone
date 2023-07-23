ffc2bf8ae2b9b3f4ca58c35134d7d24fd3f4116f
KevinMCJ <kevin72309@gmail.com>
2023-07-23 14:37:00 -0300

    Mejora en la navegacion de la lista con el teclado

--- Front/src/Components/SearchBar/SearchBar.jsx
+++ Front/src/Components/SearchBar/SearchBar.jsx
@@ -10,6 +10,7 @@ import { useNavigate } from "react-router-dom";
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [highlightedIndex, setHighlightedIndex] = useState(-1);
   const [hideList, setHideList] = useState(false);
   const inventory = useSelector((state) => state.inventory);
   const navigate = useNavigate();
@@ -25,13 +26,7 @@ const SearchBar = () => {
     setSearchQuery(query);
     setSearchResults([]);
     dispatch(filterProductsByName(query.trim()));
-    if(window.location.href !== "/home") navigate("/home");
-  };
-
-  const handleKeyDown = (event) => {
-    if (event.key === "Enter") {
-      handleSearch(searchQuery);
-    }
+    if (window.location.href !== "/home") navigate("/home");
   };
 
   const handleListSuggestions = () => {
@@ -51,6 +46,31 @@ const SearchBar = () => {
     // * Solo quiero mostrar 6
     const limitedResults = filteredProducts.slice(0, 6);
     setSearchResults(limitedResults);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "ArrowDown") {
+      event.preventDefault();
+      setHighlightedIndex((prevIndex) => {
+        const newIndex = Math.min(prevIndex + 1, searchResults.length - 1);
+        setSearchQuery(searchResults[newIndex].article_name);
+        return newIndex;
+      });
+    } else if (event.key === "ArrowUp") {
+      event.preventDefault();
+      setHighlightedIndex((prevIndex) => {
+        const newIndex = Math.max(prevIndex - 1, 0);
+        setSearchQuery(searchResults[newIndex].article_name);
+        return newIndex;
+      });
+    } else if (event.key === "Enter") {
+      if (highlightedIndex > -1) {
+        handleSearch(searchResults[highlightedIndex].article_name);
+        setHighlightedIndex(-1);
+      } else {
+        handleSearch(searchQuery);
+      }
+    }
   };
 
   const handleClearSearch = () => {
@@ -82,10 +102,12 @@ const SearchBar = () => {
         {/* Mostrar resultados posibles/sugerencias según el input */}
         {!hideList && searchResults.length && (
           <ul className={styles.resultsList}>
-            {searchResults.map((item) => (
+            {searchResults.map((item, index) => (
               <li
                 key={item.id_inventory}
                 onClick={() => handleSearch(item.article_name)}
+                onMouseEnter={() => setHighlightedIndex(index)}
+                className={index === highlightedIndex ? styles.highlighted : ""}
               >
                 {item.article_name}
               </li>
