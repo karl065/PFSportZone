9f0fd796013f7d8d2d37058d9562536234bab678
KevinMCJ <kevin72309@gmail.com>
2023-07-23 12:05:31 -0300

    prettier format

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,7 +1,7 @@
-import {useEffect} from 'react';
-import {Routes, Route, useLocation} from 'react-router-dom';
-import {useDispatch} from 'react-redux';
-import {NavBar} from './Components';
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { NavBar } from "./Components";
 import {
   Landing,
   Home,
@@ -17,24 +17,26 @@ import {
   AdminNewProduct,
   UserLogin,
   AdminNewUsers,
-} from './Views';
-import {getInventory, getUsers, setLoading} from './redux/actions/actions';
-import {useState} from 'react';
+} from "./Views";
+import { getInventory, getUsers, setLoading } from "./redux/actions/actions";
+import { useState } from "react";
 
 function App() {
   const dispatch = useDispatch();
   const location = useLocation();
-  //estado para no mostrar la nav en la pagina 404
+  // * Estado para no mostrar la nav en la pagina 404
   const [errorPage, setErrorPage] = useState(true);
+
   // * Carga inicial de los datos necesarios para la app.
   useEffect(() => {
     dispatch(setLoading(true));
     dispatch(getUsers());
     dispatch(getInventory()).then(() => dispatch(setLoading(false)));
   }, [dispatch]);
+
   return (
     <div className="App">
-      {location.pathname !== '/' && errorPage && <NavBar />}
+      {location.pathname !== "/" && errorPage && <NavBar />}
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<UserLogin />} />
