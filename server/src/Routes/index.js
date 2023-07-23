a049f249f6768de3a708830b8d4d7f048dfaa927
ManuelFBS <manuelf.borrego@gmail.com>
2023-07-23 18:13:35 -0400

    1ra parte CRUD "Ventas": POST y GET

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -1,18 +1,21 @@
-const {Router} = require('express');
-const users = require('./UsersRoutes/UsersRoutes.js');
-const auth = require('./AuthRoutes/authRoutes.js');
-const persons = require('./PersonsRoutes/PersonRoutes.js');
-const inventory = require('./InventariosRoutes/InventariosRoutes.js');
-const filters = require('./FilterRoutes/FilterRoutes.js');
-const category = require('./CategoriesRoutes/CategoriesRoutes.js');
+const { Router } = require("express");
+const users = require("./UsersRoutes/UsersRoutes.js");
+const auth = require("./AuthRoutes/authRoutes.js");
+const persons = require("./PersonsRoutes/PersonRoutes.js");
+const inventory = require("./InventariosRoutes/InventariosRoutes.js");
+const filters = require("./FilterRoutes/FilterRoutes.js");
+const category = require("./CategoriesRoutes/CategoriesRoutes.js");
+const sales = require("./SalesRoutes/SalesRoutes.js");
 const router = Router();
 
 /* El código utiliza el enrutador Express para definir rutas para diferentes partes de la aplicación. */
-router.use('/users', users);
-router.use('/auth', auth);
-router.use('/inventory', inventory);
-router.use('/persons', persons);
-router.use('/filters', filters);
-router.use('/category', category);
+router.use("/users", users);
+router.use("/auth", auth);
+router.use("/inventory", inventory);
+router.use("/persons", persons);
+router.use("/filters", filters);
+router.use("/category", category);
+
+router.use("/sales", sales);
 
 module.exports = router;
