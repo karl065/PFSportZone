727ab2eb47c08e90896542b7d86b7872e5b13b83
KevinMCJ <kevin72309@gmail.com>
2023-07-24 03:06:53 -0300

    Arreglo de bugs

--- Front/src/Components/SearchBar/SearchBar.jsx
+++ Front/src/Components/SearchBar/SearchBar.jsx
@@ -1,14 +1,14 @@
-import {useState} from 'react';
-import styles from './SearchBar.module.css';
-import {useDispatch, useSelector} from 'react-redux';
+import { useState } from "react";
+import styles from "./SearchBar.module.css";
+import { useDispatch, useSelector } from "react-redux";
 import {
   filterProductsByName,
   resetDisplayedProducts,
-} from '../../redux/actions/actions';
-import {useNavigate} from 'react-router-dom';
+} from "../../redux/actions/actions";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [highlightedIndex, setHighlightedIndex] = useState(-1);
   const [hideList, setHideList] = useState(false);
@@ -18,24 +18,25 @@ const SearchBar = () => {
 
   const handleChange = (event) => {
     setSearchQuery(event.target.value);
+    if (hideList) setHideList(false);
     if (!event.target.value) dispatch(resetDisplayedProducts());
-    setTimeout(handleListSuggestions, 300);
+    setTimeout(handleListSuggestions(event.target.value), 300);
   };
 
   const handleSearch = (query) => {
     setSearchQuery(query);
     setSearchResults([]);
     dispatch(filterProductsByName(query.trim()));
-    if (window.location.href !== '/home') navigate('/home');
+    if (window.location.href !== "/home") navigate("/home");
   };
 
-  const handleListSuggestions = () => {
+  const handleListSuggestions = (query) => {
     const seen = {};
 
     // * Filtrado para la lista de sugerencias que no repitan los nombres idénticos.
     const filteredProducts = inventory.filter((product) => {
       const articleName = product.article_name.toLowerCase();
-      const search = searchQuery.toLowerCase().trim();
+      const search = query.toLowerCase().trim();
       return (
         articleName.includes(search) &&
         !seen[articleName] &&
@@ -49,23 +50,21 @@ const SearchBar = () => {
   };
 
   const handleKeyDown = (event) => {
-    if(!searchResults.length) return;
-
-    if (event.key === "ArrowDown") {
+    if (event.key === "ArrowDown" && searchResults.length) {
       event.preventDefault();
       setHighlightedIndex((prevIndex) => {
         const newIndex = Math.min(prevIndex + 1, searchResults.length - 1);
         setSearchQuery(searchResults[newIndex].article_name);
         return newIndex;
       });
-    } else if (event.key === 'ArrowUp') {
+    } else if (event.key === "ArrowUp" && searchResults.length) {
       event.preventDefault();
       setHighlightedIndex((prevIndex) => {
         const newIndex = Math.max(prevIndex - 1, 0);
         setSearchQuery(searchResults[newIndex].article_name);
         return newIndex;
       });
-    } else if (event.key === 'Enter') {
+    } else if (event.key === "Enter") {
       if (highlightedIndex > -1 && searchResults[highlightedIndex]) {
         handleSearch(searchResults[highlightedIndex].article_name);
         setHighlightedIndex(-1);
@@ -77,7 +76,7 @@ const SearchBar = () => {
 
   const handleClearSearch = () => {
     dispatch(resetDisplayedProducts());
-    setSearchQuery('');
+    setSearchQuery("");
     setSearchResults([]);
   };
 
@@ -87,16 +86,13 @@ const SearchBar = () => {
 
   return (
     <div className={styles.container}>
-      <div
-        className={styles.bar}
-        onBlur={handleBlur}
-        onFocus={() => setHideList(false)}
-      >
+      <div className={styles.bar} onBlur={handleBlur}>
         <input
           type="text"
           value={searchQuery}
           onChange={handleChange}
           onKeyDown={handleKeyDown}
+          onFocus={() => setHideList(false)}
           placeholder="Busca entre nuestros productos..."
         />
         <button onClick={handleClearSearch} className={styles.btnClear}>X</button>
@@ -109,7 +105,7 @@ const SearchBar = () => {
                 key={item.id_inventory}
                 onClick={() => handleSearch(item.article_name)}
                 onMouseEnter={() => setHighlightedIndex(index)}
-                className={index === highlightedIndex ? styles.highlighted : ''}
+                className={index === highlightedIndex ? styles.highlighted : ""}
               >
                 {item.article_name}
               </li>
@@ -117,8 +113,14 @@ const SearchBar = () => {
           </ul>
         )}
       </div>
-      <button onClick={() => handleSearch(searchQuery)} className={styles.btnSearch}>
-        <img src="https://res.cloudinary.com/dpjeltekx/image/upload/v1690157479/PF/dsbscy8syldhvnr9ppsx.png" alt="Lupa busqueda"/>
+      <button
+        onClick={() => handleSearch(searchQuery)}
+        className={styles.btnSearch}
+      >
+        <img
+          src="https://res.cloudinary.com/dpjeltekx/image/upload/v1690157479/PF/dsbscy8syldhvnr9ppsx.png"
+          alt="Lupa busqueda"
+        />
       </button>
     </div>
   );
