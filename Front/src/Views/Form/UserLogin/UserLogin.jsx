ede37aeece96098d3dc1cab7bea0eaa31550ffc5
KevinMCJ <kevin72309@gmail.com>
2023-07-26 16:30:51 -0300

    Auth terceros, cambios y modularizacion en renderizado de acuerdo al logeo

--- Front/src/Views/Form/UserLogin/UserLogin.jsx
+++ Front/src/Views/Form/UserLogin/UserLogin.jsx
@@ -1,23 +1,35 @@
 /* eslint-disable react/prop-types */
-import {useFormik} from 'formik';
-import * as Yup from 'yup';
-import styles from './UserLogin.module.css';
-import {login} from '../../../helpers/helperLogin';
-import {useNavigate} from 'react-router-dom';
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { login } from "../../../helpers/helperLogin";
+import { useNavigate } from "react-router-dom";
+import {
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import Swal from "sweetalert2";
+import { auth } from "../../../firebase/firebaseConfig";
+import { createUser } from "../../../redux/actions/actions";
+import { useDispatch } from "react-redux";
+import googleIcon from "../../../assets/google-icon.svg";
+import facebookIcon from "../../../assets/facebook-icon.svg";
+import styles from "./UserLogin.module.css";
 
 const UserLogin = () => {
   const navigate = useNavigate();
-  // Define el esquema de validación usando Yup
+  const dispatch = useDispatch();
+  // * Define el esquema de validación usando Yup
   const validationSchema = Yup.object().shape({
-    email: Yup.string().email('Not an email').required('Email required'),
-    password: Yup.string().required('Password required'),
+    email: Yup.string().email("Not an email").required("Email required"),
+    password: Yup.string().required("Password required"),
   });
 
-  // Configura Formik y su estado inicial
+  // * Configura Formik y su estado inicial
   const formik = useFormik({
     initialValues: {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     },
     validationSchema,
     onSubmit: async (values) => {
@@ -25,18 +37,74 @@ const UserLogin = () => {
     },
   });
 
+  const swalErrorAuth = (error) => {
+    if (error?.code === "auth/popup-closed-by-user") {
+      // * Usuario cerro el pop-up intencionalmente, no es necesario mostrar un swal error.
+      return;
+    }
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!. Please try again later.",
+    });
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const newUser = {
+        email: result.user.email,
+        user: result.user.displayName,
+        userStatus: true,
+        role: "Cliente",
+      };
+      await dispatch(createUser(newUser));
+
+      // * Google access token
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      localStorage.setItem("google_access_token", credential.accessToken);
+      localStorage.setItem("role", "Cliente");
+      navigate("/home");
+    } catch (error) {
+      swalErrorAuth();
+    }
+  };
+
+  const signInWithFacebook = async () => {
+    try {
+      const provider = new FacebookAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const newUser = {
+        email: result.user.email,
+        user: result.user.displayName,
+        userStatus: true,
+        role: "Cliente",
+      };
+      await dispatch(createUser(newUser));
+
+      // * Facebook access token
+      const credential = FacebookAuthProvider.credentialFromResult(result);
+      localStorage.setItem("facebook_access_token", credential.accessToken);
+      localStorage.setItem("role", "Cliente");
+      navigate("/home");
+    } catch (error) {
+      swalErrorAuth();
+    }
+  };
+
   return (
     <section>
       <div
         id="ng-login"
         className={`${styles.login_container} bg-gradient-primary`}
-        style={{background: '#42b73a', '--bs-success': '#42b73a'}}
+        style={{ background: "#42b73a", "--bs-success": "#42b73a" }}
       >
         <div className="container">
           <div className="row justify-content-center">
             <div
               className="col-md-9 col-lg-12 col-xl-10"
-              style={{width: '400px'}}
+              style={{ width: "400px" }}
             >
               <div className="card shadow-lg o-hidden border-0 my-5">
                 <div className="card-body p-0">
@@ -44,16 +112,16 @@ const UserLogin = () => {
                     <div
                       className="col-lg-6"
                       style={{
-                        borderRadius: '10px',
-                        borderColor: 'rgba(133,135,150,0)',
-                        width: '400px',
+                        borderRadius: "10px",
+                        borderColor: "rgba(133,135,150,0)",
+                        width: "400px",
                       }}
                     >
-                      <div className="p-5" style={{width: '100%'}}>
+                      <div className="p-5" style={{ width: "100%" }}>
                         <div className="text-center">
                           <h4
                             className="text-dark mb-4"
-                            style={{fontSize: '2.4rem'}}
+                            style={{ fontSize: "2.4rem" }}
                           >
                             WELCOME!
                           </h4>
@@ -70,7 +138,7 @@ const UserLogin = () => {
                               aria-describedby="emailHelp"
                               placeholder="Email"
                               name="email"
-                              style={{borderRadius: '0px'}}
+                              style={{ borderRadius: "0px" }}
                             />
                             {formik.touched.email && formik.errors.email ? (
                               <div className="text-danger">
@@ -88,7 +156,7 @@ const UserLogin = () => {
                               type="password"
                               placeholder="Password"
                               name="password"
-                              style={{borderRadius: '0px'}}
+                              style={{ borderRadius: "0px" }}
                             />
                             {formik.touched.password &&
                             formik.errors.password ? (
@@ -108,12 +176,28 @@ const UserLogin = () => {
                             disabled={!formik.isValid || formik.isSubmitting}
                             type="submit"
                             style={{
-                              background: '#42b73a',
-                              borderRadius: '0px',
+                              background: "#42b73a",
+                              borderRadius: "0px",
                             }}
                           >
                             Login
                           </button>
+                          <div className={styles.externalAuthButtons}>
+                            <button type="button"
+                              onClick={signInWithGoogle}
+                              className={styles.btnLoginGoogle}
+                            >
+                              <img src={googleIcon} alt="Google icon" />
+                              Login with Google
+                            </button>
+                            <button type="button"
+                              onClick={signInWithFacebook}
+                              className={styles.btnLoginFacebook}
+                            >
+                              <img src={facebookIcon} alt="Facebook icon" />
+                              Login with Facebook
+                            </button>
+                          </div>
                           <hr />
                         </form>
                       </div>
