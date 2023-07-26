6a0c209cd75a8b6f92c64e52a5ff1c3b3172dea5
karl065 <karlos065@hotmail.com>
2023-07-26 13:04:02 -0500

    Eliminación de Personas

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -1,7 +1,6 @@
 const {Router} = require('express');
 const users = require('./UsersRoutes/UsersRoutes.js');
 const auth = require('./AuthRoutes/authRoutes.js');
-const persons = require('./PersonsRoutes/PersonRoutes.js');
 const inventory = require('./InventariosRoutes/InventariosRoutes.js');
 const filters = require('./FilterRoutes/FilterRoutes.js');
 const category = require('./CategoriesRoutes/CategoriesRoutes.js');
@@ -13,7 +12,6 @@ const router = Router();
 router.use('/users', users);
 router.use('/auth', auth);
 router.use('/inventory', inventory);
-router.use('/persons', persons);
 router.use('/filters', filters);
 router.use('/category', category);
 router.use('/deporte', deporte);
