93362f371888cc9edfc63350d781cb8c3fe8c604
catalfacu <cataldof21@gmail.com>
2023-07-27 01:27:09 -0300

    creacion del componente review

--- Front/src/Components/SortAndFilters/SortAndFilters.jsx
+++ Front/src/Components/SortAndFilters/SortAndFilters.jsx
@@ -1,40 +1,17 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { filterProductsByStatus, orderProductsByPrice, resetDisplayedProducts } from '../../redux/actions/actions';
 import axios from 'axios';
+import { filterProductsByStatus, orderProductsByPrice, resetDisplayedProducts } from '../../redux/actions/actions';
 import Styles from './SortAndFilters.module.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faSliders} from '@fortawesome/free-solid-svg-icons';
-import  server from '../../Connections/Server';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
+import  server from '../../Connections/Server';
 export default function SortAndFilters(props) {
   const [menuView, setMenuView] = useState(false);
   const dispatch = useDispatch();
   const displayInventory = useSelector(state => state.app.displayInventory);
-
-
-//*funcion que maneja los filtros por status de los productos
-const handleFilters = async(e) => {
-  if(e.target.checked) {
-    const response = await axios.get(`${server.api.baseURL}filters/?status=${e.target.value}`);
-    dispatch(filterProductsByStatus(response.data));
-    props.setPage(1);
-  } else {
-    dispatch(resetDisplayedProducts())
-  }
-}
-
-
-//*funcion que maneja los ordenamientos de los productos por orden de valor(precio)
-  const handleOrders = (e) => {
-    if(e.target.checked) {
-      dispatch(orderProductsByPrice(e.target.value))
-      props.setPage(1);
-    } else {
-      dispatch(resetDisplayedProducts())
-    }
-  }
-
 
 //*funciones que cambian el estado para desplegar y cerrar el menu de filtros
   const handleViewFiltersAndOrdereds = () => {
@@ -59,12 +36,14 @@ const handleFilters = async(e) => {
       <div className={Styles.filters_container}>
         <button onClick={handleViewFiltersAndOrdereds} className={Styles.button_onMenu}>
         <FontAwesomeIcon icon={faSliders} className={Styles.button_onMenu}/>
-        filtros
+        filtrar por
         </button>
         <br />
         <div className={ menuView ? Styles.menu_filters_active : Styles.menuDisabled}>
           <span>
-          <button onClick={closeMenuFilters}>X</button>
+          <button onClick={closeMenuFilters}>
+          <FontAwesomeIcon icon={faArrowLeft} className={Styles.arrowButton} />
+          </button>
           <button>eliminar filtros</button>
           </span>
           <p>Filtros elegidos:</p>
