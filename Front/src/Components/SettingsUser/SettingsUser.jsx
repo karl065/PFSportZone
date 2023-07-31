b409f8e57aa415260803f80b6d4feca2073a8ceb
karl065 <karlos065@hotmail.com>
2023-07-31 11:57:00 -0500

    Actualización de Cambios Contraseña

--- Front/src/Components/SettingsUser/SettingsUser.jsx
+++ Front/src/Components/SettingsUser/SettingsUser.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import style from './SettingsUser.module.css'
-import { useNavigate } from 'react-router-dom'
+/* eslint-disable react/prop-types */
+import style from './SettingsUser.module.css';
+import {useNavigate} from 'react-router-dom';
 const SettingsUser = (props) => {
-
-  const urlimage = 'https://res.cloudinary.com/dpjeltekx/image/upload/v1690818714/PF/WhatsApp_Image_2023-07-31_at_10.39.59_uatatn.jpg'
-  const navigate = useNavigate()
+  const urlimage =
+    'https://res.cloudinary.com/dpjeltekx/image/upload/v1690818714/PF/WhatsApp_Image_2023-07-31_at_10.39.59_uatatn.jpg';
+  const navigate = useNavigate();
 
   return (
     <div className={style[`SettingsUser${props.bool}`]}>
-        <button className={style.closeBtn} onClick={props.deployMenu}>✕</button>
-        <hr />
-        <h1 style={{textAlign:'center'}}>Settings</h1>
-          <img className={style.imageuser} src={urlimage} alt="UserImage" />
-        <hr />
-        <button className={style.elementSettings} onClick={()=>{navigate('/login/resetpass')}} >Actualizar contraseña</button>
-        <hr />
+      <button className={style.closeBtn} onClick={props.deployMenu}>
+        ✕
+      </button>
+      <hr />
+      <h1 style={{textAlign: 'center'}}>Settings</h1>
+      <img className={style.imageuser} src={urlimage} alt="UserImage" />
+      <hr />
+      <button
+        className={style.elementSettings}
+        onClick={() => {
+          navigate('/resetpass');
+        }}
+      >
+        Actualizar contraseña
+      </button>
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default SettingsUser
+export default SettingsUser;
