b5dbf3b2d076a97755560937f3445a36c8c2ea01
ManuelFBS <manuelf.borrego@gmail.com>
2023-08-08 19:52:30 -0400

    Actualizacion del About

--- Front/src/Views/About/About.jsx
+++ Front/src/Views/About/About.jsx
@@ -1,53 +1,55 @@
-import React from 'react'
-import Styles from './About.module.css';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import Styles from "./About.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const integrantes = [
   {
     nombre: "nombre",
     descripcion: "texto aca",
     foto: "link de foto aca",
-    linkedin: "link aca"
+    linkedin: "link aca",
   },
   {
     nombre: "nombre",
     descripcion: "texto aca",
     foto: "link de foto aca",
-    linkedin: "link aca"
+    linkedin: "link aca",
   },
   {
     nombre: "Facundo Cataldo",
-    descripcion: "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
+    descripcion:
+      "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
     foto: "link de foto aca",
-    linkedin: "https://www.linkedin.com/in/facundo-cataldo-9a0031272/"
+    linkedin: "https://www.linkedin.com/in/facundo-cataldo-9a0031272/",
   },
   {
     nombre: "nombre",
     descripcion: "texto aca",
     foto: "link de foto aca",
-    linkedin: "link aca"
+    linkedin: "link aca",
+  },
+  {
+    nombre: "Manuel F. Borrego S.",
+    descripcion:
+      "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
+    foto: "link de foto aca",
+    linkedin:
+      "https://www.linkedin.com/in/manuel-f-borrego-sterling-555480267/",
   },
   {
     nombre: "nombre",
     descripcion: "texto aca",
     foto: "link de foto aca",
-    linkedin: "link aca"
+    linkedin: "link aca",
   },
   {
     nombre: "nombre",
     descripcion: "texto aca",
     foto: "link de foto aca",
-    linkedin: "link aca"
+    linkedin: "link aca",
   },
-  {
-    nombre: "nombre",
-    descripcion: "texto aca",
-    foto: "link de foto aca",
-    linkedin: "link aca"
-  }
 ];
-
 
 const About = () => {
   return (
@@ -55,30 +57,29 @@ const About = () => {
       <h1>desarrollado por: </h1>
       <h1 className={Styles.sportzone}>SportZone Team</h1>
 
-    <div className={Styles.cards}>
-      {
-        integrantes.length
-        && integrantes.map((int,index)=>{
-          return (
-            <article key={index} className={Styles.cardIndividual}>
-        <img src={int.foto} alt={int.nombre} />
-        <div>
-          <h3>{int.nombre}</h3>
-          <p>{int.descripcion}</p>
-          <a href={int.linkedin} target='blank'>
-          <FontAwesomeIcon icon={faLinkedin} style={{height: '40px', hover:{color : 'whitesmoke'}}} className={Styles.social}/>
-          </a>
-        </div>
-          
-      </article>
-          )
-           
-        })
-      }
-       
+      <div className={Styles.cards}>
+        {integrantes.length &&
+          integrantes.map((int, index) => {
+            return (
+              <article key={index} className={Styles.cardIndividual}>
+                <img src={int.foto} alt={int.nombre} />
+                <div>
+                  <h3>{int.nombre}</h3>
+                  <p>{int.descripcion}</p>
+                  <a href={int.linkedin} target="blank">
+                    <FontAwesomeIcon
+                      icon={faLinkedin}
+                      style={{ height: "40px", hover: { color: "whitesmoke" } }}
+                      className={Styles.social}
+                    />
+                  </a>
+                </div>
+              </article>
+            );
+          })}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default About;
