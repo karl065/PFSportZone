a72373cd7ada73359bdf1a7ff17bfbf424e068c0
KevinMCJ <kevin72309@gmail.com>
2023-08-08 17:32:04 -0300

    Put merge local with user cart

---
+++ server/src/Controllers/ControllersCarrito/putControllerCarrito.js
@@ -0,0 +1,53 @@
+const { Carrito, Inventarios } = require("../../DB.js");
+
+const mergeWithLocalCart = async (userId, localCart) => {
+  try {
+    // * Inicia una transacción para garantizar la consistencia de los datos
+    const t = await Carrito.sequelize.transaction();
+
+    const userCart = await Carrito.findOne({
+      where: { idUser: userId },
+      transaction: t,
+    });
+
+    const dispatchPromises = localCart.map(async (product) => {
+      const inventoryProduct = await Inventarios.findByPk(
+        product.id_inventory,
+        { transaction: t }
+      );
+
+      const precioPorCant = inventoryProduct.selling_price * product.cant;
+
+      await userCart.addInventarios(inventoryProduct, {
+        through: {
+          cant: product.cant,
+          precioPorUnd: inventoryProduct.selling_price,
+          precioPorCant,
+        },
+        transaction: t,
+      });
+    });
+
+    await Promise.all(dispatchPromises);
+
+    const userProductsInCart = await userCart.getInventarios({
+      transaction: t,
+    });
+    const total = userProductsInCart.reduce((tot, product) => {
+      return tot + product.CarritoInventarios.precioPorCant;
+    }, 0);
+
+    await userCart.update(
+      { cantProd: userProductsInCart.length, total },
+      { transaction: t }
+    );
+
+    await t.commit();
+
+    return userCart;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { mergeWithLocalCart };
