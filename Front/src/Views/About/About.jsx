23237d47ed4867d01e36e92c9b440a676cab194a
KevinMCJ <kevin72309@gmail.com>
2023-08-10 11:11:36 -0300

    Improvement styles about

--- Front/src/Views/About/About.jsx
+++ Front/src/Views/About/About.jsx
@@ -1,90 +1,95 @@
-import React from 'react'
-import Styles from './About.module.css';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import Styles from "./About.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const integrantes = [
   {
     nombre: "Carlos Javier Castellanos",
-    descripcion: "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
+    descripcion:
+      "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
     foto: "link de foto aca",
-    linkedin: "https://www.linkedin.com/in/carlos-castellanos-73253b25b/"
+    linkedin: "https://www.linkedin.com/in/carlos-castellanos-73253b25b/",
   },
   {
     nombre: "Kevin Jesus Mamani Capuma",
-    descripcion: "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
+    descripcion:
+      "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
     foto: "link de foto aca",
-    linkedin: "Link aca/"
+    linkedin: "https://www.linkedin.com/in/kevin-mamani-capuma-253507264/",
   },
   {
     nombre: "Diana Marcela Tabares Marin",
-    descripcion: "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
+    descripcion:
+      "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
     foto: "link de foto aca",
-    linkedin: "link aca"
+    linkedin: "link aca",
   },
   {
     nombre: "Facundo Cataldo",
-    descripcion: "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
+    descripcion:
+      "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
     foto: "link de foto aca",
-    linkedin: "https://www.linkedin.com/in/facundo-cataldo-9a0031272/"
+    linkedin: "https://www.linkedin.com/in/facundo-cataldo-9a0031272/",
   },
   {
     nombre: "Manuel Felipe Borrego Sterling",
-    descripcion: "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
+    descripcion:
+      "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
     foto: "link de foto aca",
-    linkedin: "link aca"
+    linkedin: "link aca",
   },
   {
     nombre: "Fabian Dario Parra Naranjo",
-    descripcion: "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
+    descripcion:
+      "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
     foto: "link de foto aca",
-    linkedin: "link aca"
+    linkedin: "link aca",
   },
   {
     nombre: "Roberto Saúl Hernández Velasco",
-    descripcion: "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
+    descripcion:
+      "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
     foto: "link de foto aca",
-    linkedin: "link aca"
+    linkedin: "link aca",
   },
   {
     nombre: "Wilson Alonso Collantes Aponte",
-    descripcion: "texto aca",
+    descripcion: "FullStack developer | javascript | react | redux | nodejs | express | postgresql | sequelize",
     foto: "link de foto aca",
-    linkedin: "link aca"
-  }
+    linkedin: "link aca",
+  },
 ];
-
 
 const About = () => {
   return (
     <div className={Styles.container}>
-      <h1>desarrollado por: </h1>
-      <h1 className={Styles.sportzone}>SportZone Team</h1>
+      <h1 className={Styles.title}>SportZone Team</h1>
+      <h1 className={Styles.sportzone}>desarrollado por: </h1>
 
-    <div className={Styles.cards}>
-      {
-        integrantes.length
-        && integrantes.map((int,index)=>{
-          return (
-            <article key={index} className={Styles.cardIndividual}>
-        <img src={int.foto} alt={int.nombre} />
-        <div>
-          <h3>{int.nombre}</h3>
-          <p>{int.descripcion}</p>
-          <a href={int.linkedin} target='blank'>
-          <FontAwesomeIcon icon={faLinkedin} style={{height: '40px', hover:{color : 'whitesmoke'}}} className={Styles.social}/>
-          </a>
-        </div>
-          
-      </article>
-          )
-           
-        })
-      }
-       
+      <div className={Styles.cards}>
+        {integrantes.length &&
+          integrantes.map((int, index) => {
+            return (
+              <article key={index} className={Styles.cardIndividual}>
+                <img src={int.foto} alt={int.nombre} />
+                <div>
+                  <h3>{int.nombre}</h3>
+                  <p>{int.descripcion}</p>
+                  <a href={int.linkedin} target="blank">
+                    <FontAwesomeIcon
+                      icon={faLinkedin}
+                      style={{ height: "40px", hover: { color: "whitesmoke" } }}
+                      className={Styles.social}
+                    />
+                  </a>
+                </div>
+              </article>
+            );
+          })}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default About;
