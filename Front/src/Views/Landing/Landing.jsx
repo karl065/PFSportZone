c7a519c2a5af587fd5a4b0958cc4a389a0fe7a56
KevinMCJ <kevin72309@gmail.com>
2023-08-10 10:53:37 -0300

    Landing and footer styles.

--- Front/src/Views/Landing/Landing.jsx
+++ Front/src/Views/Landing/Landing.jsx
@@ -1,24 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
-import {useNavigate, NavLink, Link} from 'react-router-dom';
-import {isLoggedIn, handleLogout} from '../../helpers/helperLogin';
-import styles from './Landing.module.css';
-import Footer from '../../Components/Footer/Footer';
-import {useDispatch, useSelector} from 'react-redux';
-import {ChatBot} from '../../Components/';
+import { useNavigate, NavLink, Link } from "react-router-dom";
+import { isLoggedIn, handleLogout } from "../../helpers/helperLogin";
+import styles from "./Landing.module.css";
+import Footer from "../../Components/Footer/Footer";
+import { useDispatch, useSelector } from "react-redux";
+import { ChatBot } from "../../Components/";
 const Landing = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const {role} = useSelector((state) => state.app.user);
+  const { role } = useSelector((state) => state.app.user);
 
   //* función para redirigir al home al momento de hacer click a "Tienda"
   const toHome = () => {
-    navigate('/home');
+    navigate("/home");
   };
   //* función para redirigir al login al momento de hacer click a "Log In"
   const toLogIn = () => {
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
@@ -27,8 +27,8 @@ const Landing = () => {
         <NavLink to="/about">
           <p>Acerca de</p>
         </NavLink>
-        {role === 'SuperUser' || role === 'Admin' ? (
-          <Link to={'/adminProducts'}>Panel Admin</Link>
+        {role === "SuperUser" || role === "Admin" ? (
+          <Link to={"/adminProducts"}>Panel Admin</Link>
         ) : null}
         {isLoggedIn() ? (
           <li onClick={() => handleLogout(navigate, dispatch)}>Salir</li>
@@ -41,16 +41,15 @@ const Landing = () => {
           <p>F&A</p>
         </NavLink> */}
       </ul>
-      <div className={styles.titles}>
+      <div className={styles.hero_box}>
         <h1>SPORTZONE</h1>
         <h3>Todo el deporte en un solo lugar...</h3>
-      </div>
-
-      <div className={styles.buttons}>
-        <button className={styles.tienda} onClick={toHome}>
-          TIENDA
-        </button>
-        {!isLoggedIn() && <button onClick={toLogIn}>Ingresar</button>}
+        <div className={styles.buttons}>
+          <button className={styles.tienda} onClick={toHome}>
+            TIENDA
+          </button>
+          {!isLoggedIn() && <button onClick={toLogIn}>Ingresar</button>}
+        </div>
       </div>
 
       <ChatBot />
